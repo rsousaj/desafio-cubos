cf8bf20b120ce6eefd6782c9298f0ad065f79792
rsousaj <rafasousa@gmail.com>
2021-02-01 19:31:18 -0300

    added Search component

---
+++ src/components/Search/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { search } from "../../store/actions";
+import { connect } from "react-redux";
+
+const SearchInput = styled.input`
+  padding: 1em;
+  margin-bottom: 1em;
+  background: #ebebeb;
+  border: none;
+  border-radius: 30px;
+  width: 100%;
+`;
+
+const Search = ({ doSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const buscar = () => {
+    doSearch(searchTerm);
+  };
+
+  return (
+    <>
+      <SearchInput
+        type="text"
+        placeholder="Busque um filme por nome, ano ou gênero"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={buscar}>Pesquisar</button>
+    </>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  doSearch: (searchTerm) => dispatch(search(searchTerm)),
+});
+
+export default connect(null, mapDispatchToProps)(Search);
